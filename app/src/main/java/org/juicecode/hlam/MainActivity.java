70ba8c39125f5cdbebbcc6ed92bfe98f854b8590
Matthew <r9A784H0$>
2020-03-17 23:11:22 +0300

    Fix: Now fab displayed

--- app/src/main/java/org/juicecode/hlam/MainActivity.java
+++ app/src/main/java/org/juicecode/hlam/MainActivity.java
@@ -70,6 +70,5 @@ public class MainActivity extends AppCompatActivity {
 
     public void showFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
-
     }
 }
