a4a5d8af7413a949da8587e1fabf4389b40b6f30
Matthew <idiyampro@gmail.com>
2020-05-04 23:12:35 +0300

    Added get yLogin method into AsyncUserApi

--- app/src/main/java/org/juicecode/telehlam/rest/user/AsyncUserApi.java
+++ app/src/main/java/org/juicecode/telehlam/rest/user/AsyncUserApi.java
@@ -10,6 +10,8 @@ import com.google.gson.JsonIOException;
 import org.juicecode.telehlam.rest.RetrofitBuilder;
 import org.juicecode.telehlam.utils.ApiCallback;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -58,7 +60,6 @@ public class AsyncUserApi {
         signIn.enqueue(new Callback<AuthInfo>() {
             @Override
             public void onResponse(Call call, Response response) {
-                Log.i("responseBody", response.toString());
                 if (response.isSuccessful()) {
                     if (response.body() != null) {
                         try {
@@ -77,6 +78,29 @@ public class AsyncUserApi {
 
             @Override
             public void onFailure(Call call, Throwable t) {
+                Log.e(TAG, String.format("Failure while sending response\n" +
+                                "Error: %s",
+                        t.getMessage()));
+            }
+        });
+    }
+
+    public void byLogin(@NonNull String login, final ApiCallback<List<User>> callback) {
+        Call<List<User>> call = userApi.byLogin(login);
+        call.enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    List<User> users = response.body();
+                    callback.execute(users);
+                } else {
+                    //TODO error handling
+                    Log.e(TAG, "Unsuccessful response at signIn");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
                 Log.e(TAG, String.format("Failure while sending response\n" +
                                 "Error: %s",
                         t.getMessage()));
