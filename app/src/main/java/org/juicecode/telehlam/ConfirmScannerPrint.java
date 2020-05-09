126c9b07912be23b0cdfaf206d2e76fc15d16ba0
fridrock <fridrix.aleksandr.2004@mail.ru>
2020-05-09 20:30:17 +0300

    adapted fingerPrint on devices without scanner

--- app/src/main/java/org/juicecode/telehlam/ConfirmScannerPrint.java
+++ app/src/main/java/org/juicecode/telehlam/ConfirmScannerPrint.java
@@ -11,26 +11,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import org.juicecode.telehlam.R;
 import org.juicecode.telehlam.utils.FingerPrintChecker;
 import org.juicecode.telehlam.utils.SharedPreferencesRepository;
+import org.juicecode.telehlam.utils.SnackbarShower;
 
 public class ConfirmScannerPrint extends Fragment {
     Button login;
-
+    LinearLayout layout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_confirm_scanner_print, container, false);
+        final View view = inflater.inflate(R.layout.fragment_confirm_scanner_print, container, false);
+        layout = view.findViewById(R.id.layout);
         login = view.findViewById(R.id.login);
-
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FingerPrintChecker checker = new FingerPrintChecker(FingerPrintChecker.IDENTIFYING_WITH_FINGERPRINT);
-                checker.checkAuth(getContext(), new SharedPreferencesRepository(getContext()));
+                FingerPrintChecker checker = new FingerPrintChecker(FingerPrintChecker.IDENTIFYING_WITH_FINGERPRINT, getContext(),new SnackbarShower(layout));
+                checker.checkAuth(new SharedPreferencesRepository(getContext()));
             }
         });
         return view;
--- app/src/main/java/org/juicecode/telehlam/utils/FingerPrintChecker.java
+++ app/src/main/java/org/juicecode/telehlam/utils/FingerPrintChecker.java
@@ -3,12 +3,16 @@ package org.juicecode.telehlam.utils;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import org.juicecode.telehlam.R;
 
@@ -23,59 +27,83 @@ public class FingerPrintChecker {
     public static final int ADDING_FINGERPRINT = 0;
     public static final int IDENTIFYING_WITH_FINGERPRINT = 1;
     private int type;
+    private Context context;
+    private SnackbarShower snackbarShower;
 
-    public FingerPrintChecker(int type) {
+    public FingerPrintChecker(int type, Context context, SnackbarShower snackbarShower) {
         this.type = type;
+        this.context = context;
+        this.snackbarShower = snackbarShower;
     }
 
-    public void checkAuth(final Context context, SharedPreferencesRepository repository){
+    public void checkAuth(SharedPreferencesRepository repository) {
         executor = ContextCompat.getMainExecutor(context);
-        fragmentManagerSimplifier = (FragmentManagerSimplifier)context;
+        fragmentManagerSimplifier = (FragmentManagerSimplifier) context;
         classRepository = repository;
-        biometricPrompt = new BiometricPrompt((FragmentActivity) context,
-                executor, new BiometricPrompt.AuthenticationCallback() {
-            @Override
-            public void onAuthenticationError(int errorCode,
-                                              @NonNull CharSequence errString) {
-                super.onAuthenticationError(errorCode, errString);
+        if (checkBiometricAvailable()) {
+            biometricPrompt = new BiometricPrompt((FragmentActivity) context,
+                    executor, new BiometricPrompt.AuthenticationCallback() {
+                @Override
+                public void onAuthenticationError(int errorCode,
+                                                  @NonNull CharSequence errString) {
+                    super.onAuthenticationError(errorCode, errString);
 
-            }
-
-            @Override
-            public void onAuthenticationSucceeded(
-                    @NonNull BiometricPrompt.AuthenticationResult result) {
-                super.onAuthenticationSucceeded(result);
-                classRepository.saveFingerPrint();
-                fragmentManagerSimplifier.addFragment(R.id.nav_home);
-                String text;
-                if(type==ADDING_FINGERPRINT){
-                    text = "You added fingerprint!";
-                } else {
-                    text = "confirmed";
                 }
-                Toast.makeText(context,text ,
-                        Toast.LENGTH_SHORT)
-                        .show();
-            }
 
-            @Override
-            public void onAuthenticationFailed() {
-                super.onAuthenticationFailed();
-                Toast.makeText(context, "Authentication failed",
-                        Toast.LENGTH_SHORT)
-                        .show();
-            }
-        });
+                @Override
+                public void onAuthenticationSucceeded(
+                        @NonNull BiometricPrompt.AuthenticationResult result) {
+                    super.onAuthenticationSucceeded(result);
+                    classRepository.saveFingerPrint();
+                    fragmentManagerSimplifier.addFragment(R.id.nav_home);
+                    String text;
+                    if (type == ADDING_FINGERPRINT) {
+                        text = "You added fingerprint!";
+                    } else {
+                        text = "confirmed";
+                    }
+                    Toast.makeText(context, text,
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
 
-        promptInfo = new BiometricPrompt.PromptInfo.Builder()
-                .setTitle("Biometric login")
-                .setSubtitle("Log in using your biometric")
-                .setNegativeButtonText("go back")
-                .build();
-        biometricPrompt.authenticate(promptInfo);
-        // Prompt appears when user clicks "Log in".
-        // Consider integrating with the keystore to unlock cryptographic operations,
-        // if needed by your app.
+                @Override
+                public void onAuthenticationFailed() {
+                    super.onAuthenticationFailed();
+                    Toast.makeText(context, "Authentication failed",
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
+            });
 
+            promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("TELEhlam")
+                    .setSubtitle("COnfirm fingerprint to continue")
+                    .setNegativeButtonText("Cancel")
+                    .build();
+            biometricPrompt.authenticate(promptInfo);
+            // Prompt appears when user clicks "Log in".
+            // Consider integrating with the keystore to unlock cryptographic operations,
+            // if needed by your app.
+
+        }
+    }
+
+    public boolean checkBiometricAvailable() {
+        BiometricManager biometricManager = BiometricManager.from(context);
+
+        switch (biometricManager.canAuthenticate()) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                return true;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                snackbarShower.showSnackbar("This feature is not available on your device");
+                return false;
+
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                snackbarShower.showSnackbar("You do not have any biometrics, add them in your device settings");
+                return false;
+        }
+        return false;
     }
 }
